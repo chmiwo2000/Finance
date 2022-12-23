a8ac568f4a50cf14e6194b74986199aee1eacbbd
chmiwo2000 <chmiwo2000@gmail.com>
2022-12-23 22:57:16 +0900

    yfinance 추가 후, 데이터 추출

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,20 @@
 import pandas_datareader as pdr
 import pandas as pd
+import yfinance as yf
 import numpy as np
 import finterstellar as fs
 
+ticker1 = 'TMF'
+etf_df = yf.download(ticker1, start='2020-01-01', end='2022-12-18')
+_sr = etf_df['Close']
+etf_df = pd.DataFrame(_sr)
+etf_df.rename(columns={'Close':ticker1}, inplace=True)
+etf_df.head(3)
+
+
+ticker2 = '^TYX'
+rate_df = yf.download(ticker2, start='2020-01-01', end='2022-12-18')
+_sr = rate_df['Close']
+rate_df = pd.DataFrame(_sr)
+rate_df.rename(columns={'Close':ticker2}, inplace=True)
+rate_df.head(3)
