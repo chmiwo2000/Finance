51ef63b88bebcf5d4436276fa1c8478122108863
chmiwo2000 <chmiwo2000@gmail.com>
2022-12-26 16:36:15 +0900

    주석 추가 기입

--- portfolio.py
+++ portfolio.py
@@ -1,9 +1,11 @@
+# 필요한 라이브러리 로드
 import pandas_datareader as pdr
 import pandas as pd
 import yfinance as yf
 import numpy as np
 import finterstellar as fs
 
+# 미국 +20년물 채권 ETF 데이터 로드
 ticker1 = 'TMF'
 etf_df = yf.download(ticker1, start='2020-01-01', end='2022-12-18')
 _sr = etf_df['Close']
@@ -11,7 +13,7 @@ etf_df = pd.DataFrame(_sr)
 etf_df.rename(columns={'Close':ticker1}, inplace=True)
 etf_df.head(3)
 
-
+# 미국 30년물 시장금리 데이터 로드
 ticker2 = '^TYX'
 rate_df = yf.download(ticker2, start='2020-01-01', end='2022-12-18')
 _sr = rate_df['Close']
