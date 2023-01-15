971e9981b61ec33907a20ab7866387df74fc11e5
chmiwo2000 <chmiwo2000@gmail.com>
2023-01-15 20:33:24 +0900

    텔레그램 연결, ECOS로 국채금리에 대한 그래프 구현, 별로로 정의에 대한 파이썬 파일 분리

--- portfolio.py
+++ portfolio.py
@@ -1,47 +1,13 @@
-# 필요한 라이브러리 로드
-import pandas_datareader as pdr
-import pandas as pd
-import yfinance as yf
-import numpy as np
-import finterstellar as fs
-import matplotlib.pyplot as plt
-
-
-# 한국은행 통계 자료 가져오는 API
-import requests
-import xml.etree.ElementTree as ET
-import xml.dom.minidom
-
-# # 한국은행 자료 제대로 받아올 수 있는지 테스트 해보는 구간
-# url = "http://ecos.bok.or.kr/api/StatisticItemList/5H6P7H8QSOH3MNPO08UZ/xml/kr/1/1/901Y009"
-# response = requests.get(url)
-#
-# if response.status_code == 200:
-#     try:
-#         contents = response.text
-#         ecosRoot = ET.fromstring(contents)
-#
-#         if ecosRoot[0].text[:4] in ("INFO", "ERRO"):
-#             print(ecosRoot[0].text + ":" + ecosRoot[1].text)
-#         else:
-#             dom = xml.dom.minidom.parseString(contents)
-#             pretty_xml_as_string = dom.toprettyxml(indent="    ")
-#             print(pretty_xml_as_string)
-#     except Exception as e:
-#         print(str(e))
-
-
-# 아직 한국은행 자료 받아오는 구간임
+# 한국은행 자료 받아오는 구간임
 import pandas as pd
 from pandas.io.json import json_normalize
 from bs4 import BeautifulSoup
-import openpyxl
 import requests
 
 key = "5H6P7H8QSOH3MNPO08UZ"
-n = '10'
-date1 = '1990'
-date2 = '2022'
+n = '100000'
+date1 = '20100101'
+date2 = '20221231'
 
 
 def get_ECOS(key, n, date1, date2, item_code1, interval, item_code2, item_code3):
@@ -80,8 +46,7 @@ def get_ECOS(key, n, date1, date2, item_code1, interval, item_code2, item_code3)
                       pd.DataFrame(time),
                       pd.DataFrame(value),
                       pd.DataFrame(unit_name)], axis = 1)
-
-    data.columns = ['항목코드', '통계코드', '세부통계코드', '항목명', '세부통계명', '국가', '조회시점', '값', '단위']
+    data.columns = ['통계코드', '세부통계코드', '코드명', '항목명', '세부항목명', '계정명', '기간', '값', '단위']
     return data
 
 
@@ -89,23 +54,15 @@ def get_ECOS(key, n, date1, date2, item_code1, interval, item_code2, item_code3)
 
 # 이제 위의 정의된 함수에 따라서 받아올 수 있는 값들을 정의한 자료 더미들을 만든다.
 # 연간으로 조회할 지표
-combination_ECOS_A = [['902Y015', 'A', 'KOR', ''], # 경제성장률
-                      ['901Y027', 'A', 'I61E', 'I28A'],
-                      ['731Y004', 'A', '0000001', '0000100'],
-                      ['801Y002', 'A', '401000A', ''],
-                      ['901Y054', 'A', 'MO3AB', 'AB'],
-                      ['901Y054', 'A', 'MO3AA', 'AB'],
-                      ['902Y002', 'A', '3010101', ''],
-                      ['722Y001', 'A', '0101000', ''],
-                      ['121Y006', 'A', 'BECBLA0202', '']
-                      ]
+combination_ECOS = [['817Y002', 'D', '010190000', '']]
 
 
+# 위에서 검색한 리턴된 데이터 값을 다시 받아서 '엑셀'로 저장을 하기 위한 코드
 df_ecos_a = pd.DataFrame()
 df_ecos_a.to_excel('C:/Users/chmiw/Desktop/Programming/Finance/ECOS.xlsx', sheet_name='Sheet0')
 a = 1
 
-for i in combination_ECOS_A:
+for i in combination_ECOS:
     data = get_ECOS(key, n, date1, date2, i[0], i[1], i[2], i[3])
     data["값"] = pd.to_numeric(data['값'])
     df_ecos_a = df_ecos_a.append(data, ignore_index=True)
@@ -115,9 +72,77 @@ for i in combination_ECOS_A:
 print(df_ecos_a)
 
 
+# for문에서 오류가 날 경우를 대비한 검증용
+j = combination_ECOS[0]
+test_url = f"http://ecos.bok.or.kr/api/StatisticSearch/{key}/xml/kr/1/{n}/{j[0]}/{j[1]}/{date1}/{date2}/{j[2]}/{j[3]}"
+print(test_url)
+# 해당 URL을 클릭해서 정상적으로 페이지가 이동되는지 확인
+# 정상적으로 이동되었다면, 컬럼이 맞는지 확인하기 위해서 조작
+test_data = get_ECOS(key, n, date1, date2, j[0], j[1], j[2], j[3])
+test_data.head()
+
+# ECOS 홈페이지 내 통계코드 검색에서 확인
+# ['902Y015', 'A', 'KOR', ''],
+# ['901Y027', 'A', 'I61E', 'I28A'],
+# ['731Y004', 'A', '0000001', '0000100'],
+# ['801Y002', 'A', '401000A', ''],
+# ['901Y054', 'A', 'MO3AB', 'AB'],
+# ['901Y054', 'A', 'MO3AA', 'AB'],
+# ['902Y002', 'A', '3010101', ''],
+# ['722Y001', 'A', '0101000', ''],
+# ['121Y006', 'A', 'BECBLA0202', '']
+
+
+# 특정 기간 동안 국고채(1, 2, 3, 5, 10, 20, 30, 50년)금리 일별 자료 뽑아서 그래프로 표현
+rate_ECOS = [
+    ['817Y002', 'D', '010190000', ''],
+    ['817Y002', 'D', '010195000', ''],
+    ['817Y002', 'D', '010200000', ''],
+    ['817Y002', 'D', '010200001', ''],
+    ['817Y002', 'D', '010210000', ''],
+    ['817Y002', 'D', '010220000', ''],
+    ['817Y002', 'D', '010230000', ''],
+    ['817Y002', 'D', '010240000', '']
+]
+rate_data = pd.DataFrame()
+
+for k in rate_ECOS:
+    data = get_ECOS(key, n, date1, date2, k[0], k[1], k[2], k[3])
+    data['값'] = pd.to_numeric(data['값'])
+    rate_data = pd.concat([rate_data, data[['기간', '세부항목명', '값']]])
+    print(data['세부항목명'][0])
+
+
+# 각 자료를 뽑아서 년도수 별로 그림 그리는 것까지는 확인함
+# 이제 남은 단계는 각 년도수에 대한 레이블을 추가하고 앞의 일자를 공유해서 concat을 통해
+# 열로 붙여나가는 것이다. 기간을 통일하고, 열 방향으로 데이터를 뻗게해서 그래프로 그려낸다.
+
+import matplotlib.pyplot as plt
+
+x = rate_data[rate_data['세부항목명'] == '국고채(1년)']['기간']
+y1 = rate_data[rate_data['세부항목명'] == '국고채(1년)']['값']
+y1 = rate_data[rate_data['세부항목명'] == '국고채(1년)']['값']
+y2 = rate_data[rate_data['세부항목명'] == '국고채(2년)']['값']
+y3 = rate_data[rate_data['세부항목명'] == '국고채(3년)']['값']
+y5 = rate_data[rate_data['세부항목명'] == '국고채(5년)']['값']
+y10 = rate_data[rate_data['세부항목명'] == '국고채(10년)']['값']
+plt.plot(y1)
+plt.plot(y2)
+plt.plot(y3)
+plt.plot(y5)
+plt.plot(y10)
+plt.show()
 
 
 
+
+# 필요한 라이브러리 로드
+import pandas_datareader as pdr
+import pandas as pd
+import yfinance as yf
+import numpy as np
+import finterstellar as fs
+import matplotlib.pyplot as plt
 
 
 # 시작날짜 및 종료일자 설정
@@ -228,3 +253,42 @@ plt.plot(MA_40)
 plt.plot(RSI)
 plt.plot(TMF)
 plt.show()
+
+
+# 원래는 investpy가 되어야 하지만, 현재 상황에 따라서 되지 않고 있음
+# github 공식 사이트를 통해서 다시 알려줄 것 같음
+import investpy as ip
+df = ip.get_stocks("south korea")
+df.head()
+df = ip.get_stock_historical_data(stock='066570',
+                             country='south korea',
+                             from_date='01/01/2022',
+                             to_date='31/12/2022')
+
+ip.stocks.get_stock_dividends('001770', 'south korea')
+
+import telegram
+
+TELEGRAM_TOKEN = '5887961182:AAGFeUsf1tFXGDE25y6GIklHcJM6dtTAloQ'
+CHAT_ID = '5701817724'
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+updates = bot.getUpdates()
+
+for update in updates:
+    print(update.message)
+
+import asyncio
+async def main():
+    token = TELEGRAM_TOKEN
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    await bot.send_message(CHAT_ID, '매수 타이밍 입니다.')
+
+# --------
+async def chat_id():
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    updates = await bot.getUpdates()
+    print(updates)
+
+asyncio.run(chat_id())
+asyncio.run(main())
