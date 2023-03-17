7397b38e75b6646f4111d64484fd247966aa50a4
chmiwo2000 <chmiwo2000@gmail.com>
2023-03-17 12:30:45 +0900

    텔레그램까지 확인한 상태임

--- alert_finance.py
+++ alert_finance.py
@@ -10,17 +10,20 @@ import asyncio
 
 CHAT_ID = '5701817724'
 
+
 async def alarm_TMF():
     token = '6104850765:AAEW-6O9zQh91XAsDBsk9BYlE8Gkp6XsfK8'
     bot = telegram.Bot(token = token)
     async with bot:
-        await bot.send_message(CHAT_ID, '매수타이밍 입니다.')
+        await bot.send_message(CHAT_ID, '현재 TMF가 5% 하락한 상태 입니다.')
 
-asyncio.run(main())
+asyncio.run(alarm_TMF())
+
+
 
 # 시작날짜 및 종료일자 설정
 start_date = '2022-03-10'
-end_date = '2023-02-10'
+end_date = '2023-03-17'
 
 # 미국 +20년물 채권 ETF 데이터 로드
 ticker1 = 'TMF'
